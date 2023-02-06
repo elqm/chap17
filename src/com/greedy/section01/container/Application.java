80dd80d68cb28ebaaec2add0fab9c0e5552aa2f5
Jinho Yeom <yeomjh2000@gmail.com>
2023-02-06 09:24:56 +0900

    GUI

---
+++ src/com/greedy/section01/container/Application.java
@@ -0,0 +1,16 @@
+package com.greedy.section01.container;
+
+import com.greedy.section02.layout.F_PanelLayout;
+
+public class Application {
+
+	public static void main(String[] args) {
+		
+//		new JFrameTest1();
+		
+//		new JFrameTest2().showMainFrame();
+		new F_PanelLayout();
+
+	}
+
+}
